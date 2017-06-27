2e471166a661f488c5e87de4f46bb73bcead17a4
Julien Palard <julien@palard.fr>
2017-06-27 11:39:36 +0200

    Allow to configure everything (usefull to log on /dev/log typically).

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 var startPM2 = function(conf) {
     var pm2       = require('pm2');
     var SysLogger = require('ain2');
-    var sysLoggerSettings = {
-        tag: 'pm2',
-        facility: 'user'
-    };
-    if (conf.hostname) {
-        sysLoggerSettings.hostname = conf.hostname;
+
+    if (conf.port) {
+        conf.port = parseInt(conf.port, 10);
     }
-    var logger = new SysLogger(sysLoggerSettings);
+    var logger = new SysLogger(conf);
+
     pm2.launchBus(function(err, bus) {
         bus.on('*', function(event, data) {
             if (event == 'process:event') {
-                logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
-                            data.process.name,
-                            data.process.pm_id,
-                            data.process.restart_time,
-                            data.event);
+                if (conf.format == "text") {
+                    logger.warn('app=pm2 target_app=%s target_id=%s restart_count=%s status=%s',
+                                data.process.name,
+                                data.process.pm_id,
+                                data.process.restart_time,
+                                data.event);
+                } else {
+                    logger.warn(JSON.stringify(data));
+                }
             }
         });
 
         bus.on('log:err', function(data) {
-            logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+            if (conf.format == "text") {
+                logger.error('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+            } else {
+                logger.error(JSON.stringify(data));
+            }
         });
 
         bus.on('log:out', function(data) {
-            logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+            if (conf.format == "text") {
+                logger.log('app=%s id=%s line=%s', data.process.name, data.process.pm_id, data.data);
+            } else {
+                logger.error(JSON.stringify(data));
+            }
         });
     });
 };
